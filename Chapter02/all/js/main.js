23163236793169a5e173e4afc55f84613552f92d
Accio26 <acc1iela@gmail.com>
2020-01-01 18:01:21 +0900

    main.js Fix

---
+++ Chapter02/all/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('#typo').css('color', '#ebc000');
+});
+
+$(function () {
+  $('#typo .inner').css('transform', 'rotate(5deg)');
+});
+
+$(function () {
+  $('.page-main > div:nth-child(1) .inner').css('opacity', .5);
+});
+
+// 基本的な構文
+$('#typo').css('color', #ebc000);
