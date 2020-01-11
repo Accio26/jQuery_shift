a787ca21029b52e53ab3656838074b65d785c4da
Accio26 <acc1iela@gmail.com>
2020-01-11 21:42:49 +0900

    Chapter04-04 completed.

--- Chapter04/04/js/main.js
+++ Chapter04/04/js/main.js
@@ -1,19 +1,44 @@
-$(function(){
-    // 
+// $(function(){
+//     // 
+//     var duration = 300;
+
+//     // aside ----------------------------------------
+//     var $aside = $('.page-main > aside');
+//     var $asidButton = $aside.find('button')
+//         .on('click', function(){
+//             $aside.toggleClass('open');
+//             if($aside.hasClass('open')){
+//                 $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+//                 $asidButton.find('img').attr('src', 'img/btn_close.png');
+//             }else{
+//                 $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+//                 $asidButton.find('img').attr('src', 'img/btn_open.png');
+//             };
+//         });
+
+// });
+
+// サイドバーのJavaScript
+$(function () {
     var duration = 300;
 
-    // aside ----------------------------------------
+    // aside
     var $aside = $('.page-main > aside');
-    var $asidButton = $aside.find('button')
-        .on('click', function(){
+    var $asideButton = $aside.find('button')
+        .on('click', function () {
             $aside.toggleClass('open');
-            if($aside.hasClass('open')){
-                $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
-                $asidButton.find('img').attr('src', 'img/btn_close.png');
-            }else{
-                $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
-                $asidButton.find('img').attr('src', 'img/btn_open.png');
+            if ($aside.hasClass('open')) {
+                $aside.stop(true).animate({
+                    left: '-70px'
+                }, duration, 'easeOutBack');
+                $asideButton.find('img')
+                    .attr('src', 'img/btn_close.png');
+            } else {
+                $aside.stop(true).animate({
+                    left: '-350px'
+                }, duration, 'easeInBack');
+                $asideButton.find('img')
+                    .attr('src', 'img/btn_open.png');
             };
         });
-
 });
