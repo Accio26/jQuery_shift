f4c88183d8f914047052e016cebdf88deb010edb
Accio26 <acc1iela@gmail.com>
2020-02-21 23:45:42 +0900

    chapter04-01 update

--- Chapter04/01/js/main.js
+++ Chapter04/01/js/main.js
@@ -1,47 +1,43 @@
-$(function(){
-    // 
+$(function () {
     var duration = 300;
 
-    // buttons1 ----------------------------------------
-    // buttons1  1行目
-    $('#buttons1 button:nth-child(-n+4)')
-        .on('mouseover', function() {
+    $('#buttons1 button:nth-child(-n+4)') // 要素内の1~4番目の要素
+        .on('mouseover', function () {
             $(this).stop(true).animate({
                 backgroundColor: '#ae5e9b',
                 color: '#fff'
             }, duration);
         })
-        .on('mouseout', function(){
+        .on('mouseout', function () {
             $(this).stop(true).animate({
                 backgroundColor: '#fff',
                 color: '#ebc000'
             }, duration);
         });
-
-    // buttons1  2行目
-    $('#buttons1 button:nth-child(n+5):nth-child(-n+8)')
-        .on('mouseover', function(){
+    // 2行目のボタン
+    $('#buttons1 button:nth-child(n+5):nth-child(-n+8)') // 要素内の5番目から8番目の要素
+        .on('mouseover', function () {
             $(this).stop(true).animate({
                 borderWidth: '12px',
                 color: '#ae5e9b'
             }, duration, 'easeOutSine');
         })
-        .on('mouseout', function(){
+        .on('mouseout', function () {
             $(this).stop(true).animate({
                 borderWidth: '0px',
                 color: '#ebc000'
             }, duration, 'easeOutSine');
         });
-
-    // buttons1  3行目
+    // 3行目のボタン
     $('#buttons1 button:nth-child(n+9)')
-        .on('mouseover', function(){
-            $(this).find('> span')
-                .stop(true).animate({width: '100%'}, duration, 'easeOutQuad');
+        .on('mouseenter', function () {
+            $(this).find('> span').stop(true).animate({
+                width: '100%'
+            }, duration, 'easeOutQuad');
         })
-        .on('mouseout', function(){
-            $(this).find('> span')
-                .stop(true).animate({width: '0%'}, duration, 'easeOutQuad');
+        .on('mouseleave', function () {
+            $(this).find('> span').stop(true).animate({
+                width: '0%'
+            }, duration, 'easeOutQuad');
         });
-
 });
